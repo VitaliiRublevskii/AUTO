1bb0043b51a408ae86898bb52a4b1d58b831ce98
Виталий Рублевский <vitrub76@gmail.com>
2024-01-12 11:53:09 +0200

    Добавьте файлы проекта.

---
+++ Lesson_11_01_2024/Auto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//2) описать класс -авто- (марка - модель - обьем - привод - коробка). 
+//    создать список машин и вывести с него только те, у которых в 
+//    имени есть буква -а- или -а-
+
+
+namespace Lesson_11_01_2024
+{
+    public class Auto
+    {
+        public string CarBrend { get; set; }
+        public string Model { get; set; }
+        public int EngineCapacity { get; set; }
+        public string WheelDrive { get; set; }
+        public string Transmission { get; set;}
+
+        public Auto() { }
+        public Auto(string carBrend, string model, int engineCapacity, string wheelDrive, string transmission)
+        {
+            CarBrend = carBrend;
+            Model = model;
+            EngineCapacity = engineCapacity;
+            WheelDrive = wheelDrive;
+            Transmission = transmission;
+        }
+
+        public void CarShow ()
+        {
+            Console.WriteLine(  $"Автомобыль иарки {CarBrend} модели {Model}, имеет ебъем двигателя {EngineCapacity} л.," +
+                $"{WheelDrive} привод и {Transmission} коробку передач");
+        }
+    }
+}
---
+++ Lesson_11_01_2024/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System.Security.AccessControl;
+using Lesson_11_01_2024;
+
+List<string> auto = new List<string> { "AUDI", "Honda", "LEXUS", "BMW", "Volkwagen", 
+    "Mersedes", "Toyota", "Ferrari", "Infinity", "KIA", "Acura", "Alfa Romeo"};
+
+var res = auto.FindAll(x => x.Contains('a') || x.Contains('A'));
+foreach (var item in res)
+    Console.Write($"{item}, ");
+
+
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+
+Auto auto1 = new Auto("Audi", "Q7", 3, "полный", "автомат");
+Auto auto2 = new Auto("Audi", "A8", 3, "полный", "автомат");
+Auto auto3 = new Auto("BMW", "M8 Gran Coupe", 4, "полный", "автомат");
+Auto auto4 = new Auto("BMW", "5 Series", 2, "задний", "автомат");
+Auto auto5 = new Auto("Lexus", "RX", 2, "полный", "автомат");
+
+List<Auto> autos = new List<Auto>();
+autos.Add(auto1);
+autos.Add(auto2);
+autos.Add(auto3);
+autos.Add(auto4);
+autos.Add(auto5);
+
+foreach (var item in autos)
+    item.CarShow();
+Console.WriteLine(  );
+Console.WriteLine(  );
+
+var res1 = autos.FindAll(x => x.CarBrend.Contains('a') || x.CarBrend.Contains('A'));
+
+foreach (var item in res1)
+    item.CarShow();
+
+
+
